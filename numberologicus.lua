7e169a89169c8f53cd89626bf56b9f8ac186d1af
Rain_Bot <77752218+rainbot-ch@users.noreply.github.com>
2021-02-27 06:37:45 +0100

    Create numberologicus.lua

---
+++ numberologicus.lua
@@ -0,0 +1,299 @@
+                                                                   -- ..,.~:::::-..                                        
+                                                                -- .-;=$#$$$$$#@@#=!~.                                     
+                                                              -- .-!#$!:~....,~;!$@@#=:,                                   
+                                                             -- ,*#$~,          .,;@@@@$;                                  
+                                                            -- :@#:.               ,=@@@@*.                                
+                                                          -- .;@=-               .. .!@@@@$-                               
+                                                         -- ,*#*,                 ,...;@@@@$-                              
+                                                        -- .*#:                    .,. ;@@@@$~                             
+                                                       -- .*#:                      .,..;@@@@#-                            
+                                                       -- :@;                        ,- .$@@@@=.                           
+                                                      -- ;@*           ..,,..        .-, -#@@@@!.                          
+                                                     -- ~#$,         -;=$@#$=;-.      ,-. !@@@@#-                          
+                                                    -- -$@-        .;$@@@@@@@@#!,      -- -$@@@@*.                         
+                                                   -- .!@:        -=@@@@@@@@@@@@=,     ,-. *@@@@@-                         
+                                                   -- ~@#.       ,$@@@@@@@@@@@@@@=     .-, ~@@@@@$                         
+                                                  -- .$@-       ,$@@@@@@@@@@@@@@@@;     ,-..$@@@@@-                        
+                                                  -- ;@=.      .=@@@@@@@@@@@@@@@@@@-    ,-, !@@@@@=                        
+                                                 -- .@#:       *@@@@@@@@@@@@@@@@@@@$.   .-, ,@@@@@#.                       
+                                                 -- !@=.      :@@@@@@@#*:~;#@@@@@@@@;    --. $@@@@#:                       
+                                                -- .#@*       #@@@@@@=.    .=@@@@@@@=.   --. *@@@@@*                       
+                                                -- ;@#,      =@@@@@#;        =@@@@@@$-   ,-. ~@@@@@=.                      
+                                                -- =@$      ,$@@@@@;         ,$@@@@@@;   ,-, ,@@@@@$-                      
+                                               -- -$@:      :@@@@@*.          ~@@@@@@!   .-, .#@@@@#:                      
+                                               -- ;@@      .*@@@@#-           .#@@@@@=,  .-- .#@@@@@;                      
+                                               -- ;@@      -@@@@@!             !@@@@@#-  .--  $@@@@@;                      
+                                              -- ,=@=      ~@@@@#~    ,,.,.    ~@@@@@@~  .-- .$@@@@@!.                     
+                                              -- -#@-      !@@@@=.   ,.   ,.   -@@@@@@~   -- .$@@@@@*.                     
+                                              -- ~@@.     .@@@@@*   ..    .-   -#@@@@@~  .--. $@@@@@*.                     
+  -- :$#$!;;;;:-----,                            ~@@.     .@@@@@~   ,   .  .   -#@@@@@:   --. $@@@@@*.                     
+   -- !@@@@@@@@@@@@@@@@=,                        ;@@.     .@@@@#.   . . .-  .  -#@@@@@:   --. $@@@@@*.                     
+    -- !#@##@==#=$#!$#=@#;,                      !@#,....,;@@@@#---~~-.  * .,  -#@@@@@~  .--  $@@@@@*                      
+     -- :##!!!-::-!,-!-!$@=:,-------~~~~~~:;;!;;!#@$*!!!!=$###########:  =..,  -#@@@@@~  .-- .$@@@@@*.                     
+     -- .:##!!;;;:;;!=**#@@@$$$$$===$$$$==$$$$#$##$##@@@#######@@###$=-  =. .  -#@@@@#-  ,-- .#@@@@@!                      
+-- -$#####@@#$$$$$$$$$=$$$$$$$#####@@@@@@@@@@@@@@@@#===*!!@@@@@#--.-...  * ,   -@@@@@$,  ,-, ,@@@@@@;                      
+ -- :@#$$$$$$##########$$#@@@#$$$#=;!!!;~----.  .@@!      #@@@@#   .    ;- -   :@@@@@*.  ,-. -@@@@@@:                      
+ -- ~#@@##@@$!!!;:~~-,,-;$#*,.                  .@@!      ;@@@@#    ,  ,, ..  .=@@@@@!   --. *@@@@@#-                      
+-- ,:;;:~;#*--..:..:~,~=#=:                     .#@!      !@@@@#    ,    .,   ,#@@@@#:  .--  $@@@@@$.                      
+     -- ,=#!*=:*#~*#=*#@!,                      .#@!      ~@@@@#.   .,  .,    :@@@@@$,  ,-,  @@@@@@*                       
+    -- .$@@@@#@@@@#@@@=-                         =@=.     .@@@@@;    .,,.     #@@@@@=   --, ~@@@@@#:                       
+   -- ,$#######$****;:                           ;@@.     .@@@@@*            ;@@@@@@;  .--. =@@@@@#,                       
+   -- -~---.                                     :@@.     .#@@@@$-          ,#@@@@@#.  ,-, .#@@@@@=                        
+                                              -- ~@@,      !@@@@@!.        ,*@@@@@@;  .--, :@@@@@@~                        
+                                              -- ,$@:      ~@@@@@#-       .*@@@@@@#.  .--. *@@@@@#.                        
+                                              -- .*@#      ,$@@@@@@:     .*@@@@@@@~   ,-, -#@@@@@;                         
+                                               -- ;@@       ;@@@@@@@=~,-!#@@@@@@@=.  .--. !@@@@@@,                         
+                                               -- ~#@,      ,$@@@@@@@@@@@@@@@@@@$-   ,-, ,$@@@@@!                          
+                                               -- .=@=       ;@@@@@@@@@@@@@@@@@#:   ,--. !@@@@@$,                          
+                                                -- ;@#.      .*@@@@@@@@@@@@@@@@;.  .--. .#@@@@#;                           
+                                                -- ,#@*        #@@@@@@@@@@@@@@;    --,  $@@@@@=.                           
+                                                 -- :@$,       .*@@@@@@@@@@@#-    ,--. ;@@@@@@~                            
+                                                  -- #@!        .;=@@@@@@@$!,    .--, -#@@@@@!                             
+                                                  -- :##-         ,:!===;:-     .--, ,=@@@@@*.                             
+                                                   -- ;@=           ....       .,-, .*@@@@@=.                              
+                                                   -- ,=@*                     --, .*@@@@@$,                               
+                                                    -- ~$@:                  .--, .*@@@@@$-                                
+                                                     -- :@#:                .-,. -*@@@@@$-                                 
+                                                      -- :@#;.            .,,. .:$@@@@@*,                                  
+                                                       -- ~#@=,          .,.  ,*#@@@@#:.                                   
+                                                   -- .    -$@@!,           ,!@@@@@@=-                                     
+                                                         -- ,$@@#*~-,,.,,-~*@@@@@@@;.                                      
+                                                        -- -*@@@@@@##$=$#@@@@@@@@@@;                                       
+                                                       -- :$@@@$=!;#@@@@@@@@@@@@@@@#-                                      
+                                                     -- .*@@$;-..  $@@@@@@@$#@@@@@@@*.                                     
+                                                    -- -*!-.       ~@@@@@@=..;@@@@@@#:                                     
+                                                                -- -#@@@@#~   -*#@@@@=,                                    
+                                                                -- -@@@@@;      -*#@@@;                                    
+                                                                -- .@@@@=        .~=@@#,                                   
+                                                                -- .@@@#,          .~=@*                                   
+                                                                -- .@@#:             .:#-                                  
+                                                                 -- @@!.               .;                                  
+                                                                 -- #$-                                                    
+                                                                 -- #:                                                     
+                                                                 -- !  
+
+-------------------------------------------------------------------[[↑ ↑ ↓ ↓ ← → ← → B A]]
+---------------[[~~~~~~~~~~~~~start of setup variables~~~~~~~~~~~~~]]
+---------------------------------------------------------------------
+---------------[[↓ activate all hunt-modes together at once ↓]] 
+huntALL        = true  --[[← enable or disable all together ←]]
+---------------[[↓ audio feedback when a number is found]]  
+sound          = true
+---------------[[↓ coin to hunt with]]
+currency       = "btc"
+---------------[[↓ maximum chance allowed]]
+chance         = 98
+---------------[[↓ minimum required betamount for the hunt]]
+nextbet        = 0.00000100
+---------------[[↓ primary hunt on a single number with difference]]
+primary        = 0                --[[← 0 - disable | 1 - enable ←]]
+hunt           = 52.734
+---------------[[↓ secondary hunt but farthest from the hunted number]]
+secondary      = 0                   --[[← 0 - disable | 1 - enable ←]]
+farFrom        = 1--hunt
+---------------[[↓ finding the closest number to your target]]
+closeHunt      = 0          --[[← 0 - disable | 1 - enable ←]]
+closestTo      = 52.75 --99.99
+---------------[[↓ finding the farthest number from your target]]
+farHunt        = 0             --[[← 0 - disable | 1 - enable ←]]
+farthestFrom   = 52.7 --0.01
+---------------[[↓ if you want to search for specific numbers, define them on the next line between square brackets. separate each number by comma ↓]]
+arrayHunt      = 0                                                                                                  --[[← 0 - disable | 1 - enable ←]]
+numbersToHunt  = {99.99, 77.77, 0}
+---------------[[↓ if you want to search an interval, activate and define interval ↓]] 
+rangeHunt      = 0                                  --[[← 0 - disable | 1 - enable ←]]
+intervalBottom = 13.37                                  --[[← define bottom number ←]]
+intervalTop    = 14.24                                     --[[← define top number ←]]
+-------------------------------------------------------------------
+---------------[[~~~~~~~~~~~~~end of setup variables~~~~~~~~~~~~~]]
+-------------------------------------------------------------------[[↑ ↑ ↓ ↓ ← → ← → B A]]
+---------------[[↓  ↓]] 
+if (huntALL == true) then
+    primary    = 1
+    secondary  = 1
+    closeHunt  = 1
+    farHunt    = 1 
+    arrayHunt  = 1
+    rangeHunt  = 1 
+end
+bethigh        = false
+found          = false
+closest        = 999999
+closestID      = 0
+closestNummer  = 0
+farthestdiff   = 0
+farthest       = 0
+farthestID     = 0
+fardiff        = 0
+far            = 0
+farID          = 0
+farNummer      = 0
+closestdiff    = 100
+close          = 200
+closeID        = 0
+closediff      = 0
+
+function dobet()
+    
+    --------------------------------------------[[↓  ↓]]
+    --chance = math.random(9000, 11000)/1000
+    --chance = math.random(950000, 980000)/10000
+    
+        if (found == false) then i=1;for i=1,25 do print("\n")print("")end end    
+        print("\n□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□\n")
+        print("\n\t ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉  \n")    
+        print("\t         lastBet.roll: " .. lastBet.roll)
+        print("")
+        print("\t         lastBet.nonce: " .. lastBet.nonce)
+        print("\n\t ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉  \n")  
+
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   
+     
+    if (primary == 1) then    
+        evaluate = math.abs(hunt - lastBet.roll) 
+    if (evaluate <= closest) then
+        closest       = evaluate
+        closestID     = lastBet.id
+        closestNummer = lastBet.roll 
+    end
+        print("\t        -------------------------------------")
+        print("\t         closest yet: " .. closestNummer .. "\n\t         with betID b:" .. closestID .. "\n\t         at nonce: " .. lastBet.nonce .. "\n\t         missed " .. hunt .. " for " .. string.format("%.4f",closest))
+        print("\t        -------------------------------------")
+        
+    if (lastBet.roll == closest) then
+        found = true
+        print("\n███████████████████████████████████████████████████████████\n")
+        print("\tJACKPOT!!! we found: '" .. lastBet.roll .. "' with betID b:" .. lastBet.id .. " at nonce: " .. lastBet.nonce)
+        print("\n███████████████████████████████████████████████████████████\n")
+        stop() if (sound) then ching() ching() ching() end
+    end
+end
+
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    if (secondary == 1) then
+        evaluatefar = math.abs(farFrom - lastBet.roll) 
+    if (evaluatefar >= far) then
+        far = evaluatefar
+        farID = lastBet.id
+        farNummer = lastBet.roll
+    end
+        print("\t        -------------------------------------")
+        print("\t         farthest yet: " .. farNummer .. "\n\t         with betID b:" .. farID .. "\n\t         at nonce: " .. lastBet.nonce ..  "\n\t         gap to " .. farFrom .. " is " .. string.format("%.4f",far))
+        print("\t        -------------------------------------")
+        
+    if (lastBet.roll == hunt) then
+        found = true
+        print("\n███████████████████████████████████████████████████████████\n")
+        print("\tJACKPOT!!! we found: '" .. lastBet.roll .. "' with betID b:" .. lastBet.id .. " at nonce: " .. lastBet.nonce)
+        print("\n███████████████████████████████████████████████████████████\n")
+        stop() if (sound) then ching() ching() ching() end
+    end
+end
+
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    if (closeHunt == 1) then    
+    if (lastBet.roll >= closestTo) then
+        closediff = (lastBet.roll - closestTo)
+    else
+        closediff = (closestTo - lastBet.roll)
+    end
+    if (closediff <= closestdiff) then
+        closestdiff = closediff
+        close = lastBet.roll
+        closeID = lastBet.id
+    end
+        print("\t        -------------------------------------")
+        print("\t         " .. close .. " closest to " .. closestTo)
+        print("\t         closestdiff: " .. string.format("%.4f",closestdiff))
+        print("\t         b:" .. closeID)
+        print("\t        -------------------------------------")
+    end
+        
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    if (farHunt == 1) then 
+    if (lastBet.roll <= farthestFrom) then
+        fardiff = (farthestFrom - lastBet.roll)
+    else
+        fardiff = (lastBet.roll - farthestFrom)
+    end
+    if (fardiff >= farthestdiff) then
+        farthestdiff = fardiff
+        farthest = lastBet.roll
+        farthestID = lastBet.id
+    end
+        print("\t        -------------------------------------")
+        print("\t         " .. farthest .. " farthest from " .. farthestFrom)
+        print("\t         farthestdiff: " .. string.format("%.4f",farthestdiff))        
+        print("\t         b:" .. farthestID)
+        print("\t        -------------------------------------")
+    end
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    if (arrayHunt == 1) then
+    if containedInSet(lastBet.roll, numbersToHunt) then
+        found = true
+        print("\n███████████████████████████████████████████████████████████\n")
+        print("\tJACKPOT!!! we found: '" .. lastBet.roll .. "' with betID b:" .. lastBet.id .. " at nonce: " .. lastBet.nonce)
+        print("\n███████████████████████████████████████████████████████████\n")
+        stop() if (sound) then ching() ching() ching() end
+    end
+end
+
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    if (rangeHunt == 1) then
+        if lastBet.roll >= intervalBottom and lastBet.roll <= intervalTop then
+        found = true
+        print("\n███████████████████████████████████████████████████████████\n")
+        print("\tJACKPOT!!! we found: '" .. lastBet.roll .. "' with betID b:" .. lastBet.id .. " at nonce: " .. lastBet.nonce)
+        print("\n███████████████████████████████████████████████████████████\n")
+        stop() if (sound) then ching() ching() ching() end
+    end
+end
+
+--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    
+
+        print("\n\t ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉ ҉  \n")   
+        print("\n□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□\n")
+end
+
+function containedInSet(x, set)
+    for key, value in pairs(set) do
+        if x == value then
+            return true
+        end
+    end
+    return false
+end
+
+----------------------------------------------------------------------------------
+--[[¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"--
+--"////////////////////////////////////////////////////////////////////////////"--
+--"                                                                            "--
+--"  ｡● ✧｡★ﾟ (/¯◡ ‿ ◡)/¯﻿☂ made with ♥ in Switzerland ☂ ~(˘▾˘~)  ﾟ｡✩ ✧ ●    "--
+--"                                                                            "--
+--"                            --  rainbot.ch  --                              "--
+--"                                                                            "--
+--"////////////////////////////////////////////////////////////////////////////"--
+--"¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"--
+
+                        --|||||||||||||||||||||||||--
+                        --▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄--
+                        --░░░░░░░░░░░░░░░░░░░░░░░░░--
+                        --░ ▄▄▄▄▄▄▄   ▄ ▄ ▄▄▄▄▄▄▄ ░--
+                        --░ █ ▄▄▄ █ ▄ ▄ ▀ █ ▄▄▄ █ ░--
+                        --░ █ ███ █  ▄ ▄█ █ ███ █ ░--
+                        --░ █▄▄▄▄▄█ ▄▀█▀▄ █▄▄▄▄▄█ ░--
+                        --░ ▄▄▄ ▄▄▄▄█ █ ▄▄▄   ▄   ░--
+                        --░  ▀ █▄ ▄▀ ▄██  ▀▀██▄██ ░--
+                        --░ ▀▄▄▀ ▄▄█▄ ▄█▀  █▄  ▀█ ░--
+                        --░ ▄▄▄▄▄▄▄ █▀█ ▄█ ▄███▄█ ░--
+                        --░ █ ▄▄▄ █ █ ▀ ▄  ▄▀  ▀▀ ░--
+                        --░ █ ███ █ ▄▄▀█ ▀███ ▀▀▄ ░--
+                        --░ █▄▄▄▄▄█ █▄▀█▀▀▄▀█  █▄ ░--
+                        --░ _ _ _ _ _ _ _ _ _ _ _ ░--
+                        --░░░░░░░░░░░░░░░░░░░░░░░░░--
+                        --▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄]]
