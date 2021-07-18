d4547451d5ac4080cca937398e87b61652da0052
Rain_Bot <77752218+rainbot-ch@users.noreply.github.com>
2021-07-19 01:40:07 +0200

    Delete betspeed().lua

--- betspeed().lua
+++
@@ -1,3 +0,0 @@
-playtime = os.clock() --goes before dobet()
-
-print("~bets per: " .. string.format("%2.3f", bets/(os.clock()-playtime)) .. " /second | " .. math.floor(0.5+bets/(os.clock()-playtime)*60^2) .. " /hour | " .. math.floor(0.5+24*bets/(os.clock()-playtime)*60^2) .. " /day")
