2eda8df2f56ce299c67e96525cf05704aca9251a
Rain_Bot <77752218+rainbot-ch@users.noreply.github.com>
2021-10-02 09:35:49 +0200

    Create function eventually().lua

---
+++ function eventually().lua
@@ -0,0 +1,19 @@
+g={{0,0},{0,0},{0,0}}
+function eventually()
+function tara()
+    return math.randomseed(os.time());
+    end;tara();g[1][1]=(string.format("%009.0f",math.random(10^6,10^7))/5^10)*100
+    g[2][1]=(string.format("%009.0f",math.random(10^6,10^7))/5^10)*100
+    g[3][1]=(string.format("%009.0f",math.random(10^6,10^7))/5^10)*100
+    g[1][2]=math.abs(lastBet.roll-g[1][1]);
+    g[2][2]=math.abs(lastBet.roll-g[2][1])
+    g[3][2]=math.abs(lastBet.roll-g[3][1])
+function highfirst(a,b)
+    return a[2]>b[2];
+end;table.sort(g,highfirst);
+if(g[1][1]<50)then bethigh=false
+        else bethigh=true end
+    if(bethigh)then 
+   print("over")else 
+print("under")end 
+end;
