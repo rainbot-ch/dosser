53fbc20cf32894918b202742c3781dd44ffe3eeb
Rain_Bot <77752218+rainbot-ch@users.noreply.github.com>
2023-06-21 13:18:34 +0200

    Update function kip(k)i,l,o.lua

--- function kip(k)i,l,o.lua
+++ function kip(k)i,l,o.lua
@@ -1,4 +1,7 @@
-function kip(k)i,l,o=string.match(k,"^([^%d]*%d)(%d*)(.-)$")
-    return print((i)..(l):reverse():gsub("(%d%d%d)","%1'"):reverse()..(o))
+function kip(k)i,l,o=string.match(k,"^([^%d]*%d)(%d*)(.-)$");
+    return(i)..(l):reverse():gsub("(%d%d%d)","%1'"):reverse()..(tonumber(string.match(o,"%.(%d*)"))>0 and o or '')
 end;
+
+print( kip(188333523443) )
+
 --kip(math.floor(balance/basebet))
